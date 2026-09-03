07190ffc338861d771c2ec8ab4d60233dee1c307
cgonzales <gonzalesch43@hotmail.com>
2026-09-02 20:42:30 -0500

    feat: FrameScorer and Tests

---
+++ WeInfuseTakeHome/FrameScorer.cs
@@ -0,0 +1,101 @@
+﻿namespace WeInfuseTakeHome
+{
+    public class FrameScorer
+    {
+        public int?[] GetFrameScores(string[] input)
+        {
+            var frames = new List<int?>();
+            var rolls = new Queue<string>(input);
+            var curFrame = 0;
+
+            if (rolls.Count == 0)
+                return frames.ToArray();
+
+            do
+            {
+                if (rolls.TryDequeue(out var firstRoll))
+                {
+                    if (firstRoll == "X")
+                    {
+                        var bonusRolls = rolls.Take(2);
+
+                        if (bonusRolls.Count() != 2)
+                        {
+                            // Incomplete Frame - No bonus rolls
+                            frames.Add(null);
+                        }
+                        else
+                        {
+                            var scrapBonus = 0;
+                            var score = bonusRolls.Aggregate(10, (total, next) =>
+                            {
+                                if (int.TryParse(next, out var bonus))
+                                {
+                                    scrapBonus = bonus;
+                                    return total + bonus;
+                                }
+                                else
+                                {
+                                    return total + 10 - scrapBonus;
+                                }
+                            });
+                            frames.Add(score);
+                        }
+
+                        curFrame++;
+
+                        if (curFrame > 9)
+                            break;
+                        else
+                            continue;
+                    }
+                }
+
+                if (rolls.TryDequeue(out var secondRoll))
+                {
+                    if (secondRoll == "/")
+                    {
+                        if (!rolls.TryPeek(out var bonusRoll))
+                        {
+                            // Incomplete Frame - No bonus roll
+                            frames.Add(null);
+                        }
+                        else
+                        {
+                            if (int.TryParse(bonusRoll, out var bonus))
+                            {
+                                frames.Add(10 + bonus);
+                            }
+                            else
+                            {
+                                frames.Add(20);
+                            }
+                        }
+
+                        curFrame++;
+
+                        if (curFrame > 9)
+                            break;
+                        else
+                            continue;
+                    }
+                }
+                else
+                {
+                    // Incomplete Frame - No second roll
+                    frames.Add(null);
+                    curFrame++;
+                    break;
+                }
+
+                var first = int.Parse(firstRoll);
+                var second = int.Parse(secondRoll);
+
+                frames.Add(first + second);
+                curFrame++;
+            } while (rolls.Count > 0);
+
+            return frames.ToArray();
+        }
+    }
+}
---
+++ WeInfuseTakeHome/Tests.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+
+namespace WeInfuseTakeHome
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCaseSource(nameof(AddTestCases))]
+        public int?[] Returns_Correct_FrameScores_1(string[] input)
+        {
+            var scorer = new FrameScorer();
+
+            var result = scorer.GetFrameScores(input);
+
+            return result;
+        }
+
+        private static IEnumerable<TestCaseData> AddTestCases()
+        {
+            yield return new TestCaseData((object)new string[] { })
+                .SetName("Empty Game")
+                .Returns(new int?[] { });
+
+            yield return new TestCaseData((object)new string[] { "4" })
+                .SetName("Single Frame Incomplete")
+                .Returns(new int?[] { null });
+
+            yield return new TestCaseData((object)new string[] { "7", "2" })
+                .SetName("Single Frame")
+                .Returns(new int?[] { 9 });
+
+            yield return new TestCaseData((object)new string[] { "4", "5", "X", "8" })
+                .SetName("Incomplete Strike")
+                .Returns(new int?[] { 9, null, null });
+
+            yield return new TestCaseData((object)new string[] { "1", "3", "8", "/" })
+                .SetName("Incomplete Spare")
+                .Returns(new int?[] { 4, null });
+
+            yield return new TestCaseData((object)new string[] { "4", "5", "X", "8", "1" })
+                .SetName("Strike")
+                .Returns(new int?[] { 9, 19, 9 });
+
+            yield return new TestCaseData((object)new string[] { "1", "3", "8", "/", "7" })
+                .SetName("Three Frames Incomplete")
+                .Returns(new int?[] { 4, 17, null });
+
+            yield return new TestCaseData((object)new string[] { "2", "4", "9", "/", "1", "6" })
+                .SetName("Three Frames")
+                .Returns(new int?[] { 6, 11, 7 });
+
+            yield return new TestCaseData((object)new string[] { "6", "2", "X", "8", "/", "X", "5", "2" })
+                .SetName("Five Frames")
+                .Returns(new int?[] { 8, 20, 20, 17, 7 });
+
+            yield return new TestCaseData((object)new string[] { "X", "X", "X", "X", "X", "X", "X", "X", "X", "8", "1" })
+                .SetName("10th Frame")
+                .Returns(new int?[] { 30, 30, 30, 30, 30, 30, 30, 28, 19, 9 });
+
+            yield return new TestCaseData((object)new string[] { "X", "X", "X", "X", "X", "X", "X", "X", "X", "9", "/", "2" })
+                .SetName("10th Frame w/Spare")
+                .Returns(new int?[] { 30, 30, 30, 30, 30, 30, 30, 29, 20, 12 });
+
+            yield return new TestCaseData((object)new string[] { "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "2", "3" })
+                .SetName("10th Frame w/Strike")
+                .Returns(new int?[] { 30, 30, 30, 30, 30, 30, 30, 30, 22, 15 });
+
+            yield return new TestCaseData((object)new string[] { "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X" })
+                .SetName("Perfect Game")
+                .Returns(new int?[] { 30, 30, 30, 30, 30, 30, 30, 30, 30, 30 });
+        }
+    }
+}
